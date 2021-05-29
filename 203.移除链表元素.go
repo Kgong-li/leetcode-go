d7faa1545ddf9dc3743f01f4c4d6f2959dd52f1f
Kgong.Li <kgong.li@vrviu.com>
2021-05-29 18:57:45 +0800

    list

---
+++ 203.移除链表元素.go
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=golang
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	nHead := &ListNode{Val: 0, Next: head}
+	pre := nHead
+	cur := nHead.Next
+	for cur != nil {
+		if cur.Val == val {
+			pre.Next = cur.Next
+		} else {
+			pre = cur
+		}
+		cur = cur.Next
+	}
+	return nHead.Next
+}
+
+// @lc code=end
+
